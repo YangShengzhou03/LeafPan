c32207f93935906cac56be84ee456841b39d33bd
Yangshengzhou <3555844679@qq.com>
2025-10-23 10:19:55 +0800

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,12 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8080,
-    proxy: {
-      '/': {
-        target: 'http://localhost:8081',
-        changeOrigin: true
-      }
-    }
+    port: 8080
   }
 })
